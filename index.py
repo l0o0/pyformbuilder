5b3c4ad4f1d3ddd555c4e76013fa999ebbb3acb4
linxingzhong <linxingzhong@1gene.com.cn>
2017-08-21 17:33:31 +0800

    use url_for instead of url string.

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from flask import Flask, request, Response, render_template, session, redirect
+from flask import Flask, request, Response, render_template, redirect, url_for
 from config import config
 from formbuilder import formLoader
 import json
@@ -8,44 +8,47 @@ import json
 app = Flask(__name__, static_folder='src')
 
 app.secret_key = 'Sh3r1n4Mun4F'
+mysession = {}
 
 @app.route('/')
 def index():
 
-	return render_template('index.html', base_url=config['base_url'])
+    return render_template('index.html')
 
 @app.route('/save', methods=['POST'])
 def save():
-	if request.method == 'POST':
-		formData = request.form.get('formData')
+    print 'save called'
+    if request.method == 'POST':
+        formData = request.form.get('formData')
 
-		if formData == 'None':
-			return 'Error processing request'
+        if formData == 'None':
+            return 'Error processing request'
 
-		session['form_data'] = formData
+        mysession['form_data'] = formData
 
-		return 'tes'
+        return 'OK'
 
 @app.route('/render')
 def render():
-	if not session['form_data']:
-		redirect('/')
+    print 'render directed'
+    if not mysession.get('form_data'):
+        return redirect('/')
 
-	form_data = session['form_data']
-	session['form_data'] = None
+    form_data = mysession.get('form_data')
+    mysession['form_data'] = None
 
-	form_loader = formLoader(form_data, '{0}/submit'.format(config['base_url']))
-	render_form = form_loader.render_form()
+    form_loader = formLoader(form_data, url_for('submit'))
+    render_form = form_loader.render_form()
 
-	return render_template('render.html', render_form=render_form)
+    return render_template('render.html', render_form=render_form)
 
 @app.route('/submit', methods=['POST'])
 def submit():
-	if request.method == 'POST':
-		form = json.dumps(request.form)
+    if request.method == 'POST':
+        form = json.dumps(request.form)
 
-		return form
+        return form
 
 if __name__ == '__main__':
-	app.debug = True
-	app.run()
+    app.debug = True
+    app.run()
